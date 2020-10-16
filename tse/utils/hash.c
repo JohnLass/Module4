863b5b395314173f8526a7a0813179f45b3aa6e1
BobbyCarangelo <robert.f.carangelo.iii.21@dartmouth.edu>
2020-10-16 14:27:06 -0400

    10.16.20 2:26 PM EST

---
+++ tse/utils/hash.c
@@ -0,0 +1,144 @@
+/* 
+ * hash.c -- implements a generic hash table as an indexed set of queues.
+ *
+ */
+#include <stdint.h>
+#include "hash.h"
+#include "queue.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/* 
+ * SuperFastHash() -- produces a number between 0 and the tablesize-1.
+ * 
+ * The following (rather complicated) code, has been taken from Paul
+ * Hsieh's website under the terms of the BSD license. It's a hash
+ * function used all over the place nowadays, including Google Sparse
+ * Hash.
+ */
+#define get16bits(d) (*((const uint16_t *) (d)))
+
+
+typedef struct ht {
+	queue_t **qTable;
+  uint32_t hsize;
+} ht_t;
+ 
+static uint32_t SuperFastHash (const char *data,int len,uint32_t tablesize) {
+  uint32_t hash = len, tmp;
+  int rem;
+  
+  if (len <= 0 || data == NULL)
+		return 0;
+  rem = len & 3;
+  len >>= 2;
+  /* Main loop */
+  for (;len > 0; len--) {
+    hash  += get16bits (data);
+    tmp    = (get16bits (data+2) << 11) ^ hash;
+    hash   = (hash << 16) ^ tmp;
+    data  += 2*sizeof (uint16_t);
+    hash  += hash >> 11;
+  }
+  /* Handle end cases */
+  switch (rem) {
+  case 3: hash += get16bits (data);
+    hash ^= hash << 16;
+    hash ^= data[sizeof (uint16_t)] << 18;
+    hash += hash >> 11;
+    break;
+  case 2: hash += get16bits (data);
+    hash ^= hash << 11;
+    hash += hash >> 17;
+    break;
+  case 1: hash += *data;
+    hash ^= hash << 10;
+    hash += hash >> 1;
+  }
+  /* Force "avalanching" of final 127 bits */
+  hash ^= hash << 3;
+  hash += hash >> 5;
+  hash ^= hash << 4;
+  hash += hash >> 17;
+  hash ^= hash << 25;
+  hash += hash >> 6;
+  return hash % tablesize;
+}
+
+hashtable_t *hopen(uint32_t hsize) {
+  //memory allocation 
+  ht_t *ht;
+  if (!(ht=(ht_t*)malloc(sizeof(ht_t)))) {
+      printf("Error: Failure allocating memory\n");
+      return NULL;
+  }
+  if(!(ht->qTable=malloc(sizeof(void *)*hsize))){
+    printf("Error: Failure allocating memory\n");
+    return NULL;
+  }
+
+  //setting hashsize
+  ht->hsize=hsize;
+  
+  //setting queue pointers
+  queue_t **hold = ht->qTable;
+  for(int i=0; i<hsize; i++){
+    hold[i] = qopen();
+  }
+
+  return (hashtable_t*)ht;
+}
+
+void hclose(hashtable_t *htp){
+  if(htp!=NULL){
+		ht_t *ptr = (ht_t*)htp;
+		queue_t **hold = ptr->qTable;
+    for(int i=0; i<ptr->hsize; i++){
+      qclose(hold[i]);
+    }
+  
+  free(ptr->qTable);
+  free(ptr);
+	}
+}
+int32_t hput(hashtable_t *htp, void *ep, const char *key, int keylen){
+  ht_t *ptr = htp;
+  int value = 1;
+  if((ptr!=NULL) && (ep!=NULL)){
+    uint32_t pos;
+    pos = SuperFastHash(key,keylen,ptr->hsize);
+    queue_t **hold = ptr->qTable;
+    value = qput(hold[pos],ep);
+  }
+
+  return value;
+  
+}
+
+	
+
+void *hsearch(hashtable_t *htp, bool (*searchfn)(void* elementp, const void* searchkeyp), const char *key, int32_t keylen){
+	if(htp!=NULL && key!=NULL && keylen>=0){
+		ht_t *ptr = (ht_t*)htp;
+		uint32_t pos;
+		pos = SuperFastHash(key,keylen,ptr->hsize);
+		queue_t **hold = ptr->qTable;
+		void *data=(void*)key;
+		void *rtrn=qsearch(hold[pos],searchfn,data);
+		return rtrn;
+	}
+	printf("ERROR With Hash/key/keylen");
+	return NULL;
+}
+
+void happly(hashtable_t *htp, void (*fn)(void* ep)){
+	if(htp != NULL && fn != NULL){
+		ht_t *ptr = (ht_t *) htp;
+		for(int i = 0; i < ptr->hsize; i++){
+			qapply(ptr->qTable[i],fn);		
+		}
+	}
+	else{
+		printf("NULL arg\n");
+	}
+}
---
+++ tse/utils/queue.c
@@ -0,0 +1,252 @@
+/* queue.c --- 
+ * 
+ * 
+ * Author: John J. Lass, Tyler Neath, Robert Carangelo
+ * Created: Sun Oct  4 14:48:46 2020 (-0400)
+ * Version: 1.0
+ * 
+ * Description:Functions to create and manioulate queues of different data 
+ * types
+ */
+
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+typedef struct ll {
+	void *data;
+	struct ll *next;
+} ll_t;
+
+typedef struct rq {
+	struct ll *front;
+	struct ll *back;
+} rq_t;
+
+/*prints the queue*/
+void debugq(queue_t *cp){
+	if(cp!=NULL){
+		printf("Queue has been created\n");
+		rq_t *ptr = (rq_t*)cp;
+		printf("This is the front pointer: %p\n",(void *) ptr->front);
+		printf("This is the back pointer: %p\n", (void *)ptr->back);
+		
+		if(ptr->front != NULL){
+			ll_t *incp;
+			for(incp=ptr->front; incp!=NULL; incp = incp->next){
+				printf("This is the ll_t's next: %p\n",(void *)incp->next);
+				if(incp->data != NULL){
+					printf("Node's data is not empty\n");
+				}else{
+					printf("Node's data is empty\n");
+				}
+			}
+		}
+	}
+	else{
+		printf("This should point to nil %p\n", cp);
+	}
+}
+
+
+/*creates an empty queue,allocates space for a queue, returns the queue in a pointer to a queue_t*/
+queue_t* qopen(void) {
+	rq_t* ptr=(rq_t*)malloc(sizeof(rq_t));
+	ptr->front=NULL;
+	ptr->back=NULL;
+	return((queue_t*)ptr);
+}
+
+/*Deallocates a queue, frees everything in it */
+void qclose(queue_t *qp){
+	ll_t *hold=NULL;
+	if(qp!=NULL){
+		rq_t *ptr=(rq_t*)qp;
+		//if there is something in the queue
+		if(ptr->front!=NULL){
+			//loop through the queue, deallocating the memory in each node and the node itself
+			do{
+				if(ptr->front->data!=NULL){
+					free(ptr->front->data);
+				}
+				hold = ptr->front;
+				ptr->front = hold->next;
+				free(hold);
+			}	while(ptr->front != NULL);
+		}	
+		//free the queue itself
+		free(ptr);
+	}
+}
+
+/*Put element at the end of the queue
+return 0 if successful, nonzero otherwise
+allocate space in queue for new item*/
+int32_t qput(queue_t *qp, void *elementp){
+	int32_t rtrn = 1;
+	
+	if(qp!=NULL){
+		if(elementp!=NULL){
+			//if both arguments are valid, need to cast the qp and allocate space for new node
+			rq_t *ptr = (rq_t*)qp;
+			ll_t *newp;
+			if( (newp = (ll_t*) malloc(sizeof(ll_t))) == NULL)
+				return rtrn;
+			newp->data = elementp;
+			newp->next = NULL;
+			//if it isnt the first thing reassign the back thing in list to point to new one and reassign back pointer
+			if(ptr->front != NULL){
+				ptr->back->next = newp;
+				ptr->back = newp;
+			}else{
+				//set front and back to new thing
+				ptr->front = newp;
+				ptr->back = newp;			 
+			}
+			rtrn = 0;
+		}
+	}
+	return rtrn;
+}
+
+
+/*get the first element from the queue, remove it from the queue, free memory to its spot in the queue but not the actual data*/
+void* qget(queue_t *qp){
+	void *rtrn = NULL;	
+	if(qp!=NULL){
+		rq_t *ptr = (rq_t *) qp;
+		if(ptr->front != NULL){
+			ll_t *hold = NULL;
+			hold = ptr->front;
+			if(ptr->front == ptr->back)
+				ptr->back = NULL;
+			ptr->front = hold->next;
+			rtrn = hold->data;
+			free(hold);
+		}else{
+			printf("queue is empty\n");
+		}
+	}
+	return rtrn;
+}
+
+
+/*apply a function to every element in the queue*/
+void qapply(queue_t *qp, void (*fn)(void* elementp)){
+	if(qp!=NULL){
+		if(fn!=NULL){
+			rq_t *ptr = (rq_t *) qp;
+			if(ptr->front!=NULL){
+				ll_t *incp = NULL;
+				for(incp=ptr->front; incp!=NULL; incp=incp->next){
+					fn(incp->data);
+				}
+			}
+			else{
+				printf("Queue is empty!\n");	
+			}
+		}
+		else{
+			printf("Passed Null function!\n");
+		}
+	}
+		else{
+		printf("Queue does not exist!\n");
+	}
+}
+
+
+
+void* qsearch(queue_t *qp,bool (*searchfn)(void* elementp,const void* keyp),const void* skeyp){
+	if(qp != NULL){
+		rq_t *ptr=(rq_t*)qp;
+		ll_t *incp;
+		bool flag;
+
+		if(ptr->front!=NULL){
+			for(incp=ptr->front;incp!=NULL;incp=incp->next){
+				flag = searchfn(incp->data,skeyp);
+				printf("Loop\n");
+				if(flag==true){
+					printf("Element Found!\n");
+					return incp->data;
+				}
+			}
+		}
+		if(ptr->front==NULL)
+			printf("Empty List");
+		printf("Element Not Found.\n");
+		return NULL;
+	}
+	printf("Invalid arg\n");
+	return NULL;
+}
+
+
+
+void* qremove(queue_t *qp,
+							bool (*searchfn)(void* elementp,const void* keyp),
+							const void* skeyp){
+	void * rtn = NULL;
+	if(qp != NULL){
+		rq_t *ptr=(rq_t*)qp;
+		ll_t *incp, *prev, *hold;
+		bool flag = false;
+		int loop_count = 0;
+		hold = NULL;
+		
+		if(ptr->front!=NULL){
+			incp = ptr->front;
+			while(flag == false && incp != NULL){		
+				flag = searchfn(incp->data,skeyp);
+				printf("Loop\n");
+				if(flag==true){
+					printf("Element Found!\n");
+					rtn =  incp->data;
+					if(loop_count == 0){
+						ptr->front = incp->next;
+					}else{
+						prev->next = incp->next;
+					}
+					hold = incp;
+					
+					free(hold);
+				}else{
+					prev = incp;
+					incp = incp->next;
+				}
+				loop_count = loop_count +1;
+			}
+			
+		}else
+			printf("Empty List");
+		if(flag == 0)
+			printf("Element Not Found.\n");
+	}
+	return rtn;
+}
+
+
+/*combines q2 into the end of q1, then frees q2*/
+void qconcat(queue_t *q1p, queue_t *q2p){
+	if(q1p != NULL && q2p != NULL){
+		rq_t *ptr1 = (rq_t *) q1p;
+		rq_t *ptr2 = (rq_t *) q2p;
+		if(ptr2->back != NULL){
+			if(ptr1->front == NULL){
+				ptr1->front = ptr2->front;
+			}else{
+				ptr1->back->next = ptr2->front;
+			}
+			ptr1->back = ptr2->back;
+			ptr2->back = NULL;
+			ptr2->front = NULL;
+		}else{
+			printf("Second queue is empty!\n");
+		}
+		qclose(ptr2);
+	}else{
+		printf("Passed null pointer!\n");
+	}
+}

a114576137869d027c5b609fbc66ede46b660c6f
Tyler Neath <tyler.i.neath.21@dartmout.edu>
2020-10-17 17:12:57 -0400

    10/17/20 5:17pm EST

--- tse/crawler/crawler.c
+++ tse/crawler/crawler.c
@@ -14,25 +14,37 @@
 #include "queue.h"
 #include "hash.h"
 #include "webpage.h"
-
+#include <stdbool.h>
 
 int main(void){
 	char *url = "http://thayer.github.io/engs50/";
 	int depth = 0;
-	char *html =NULL;
 
-	webpage_t *w1 = webpage_new(url,depth,html);;
-	int w2;
+ 
 
+	webpage_t *w1 = webpage_new(url,depth,NULL);
 
-	w2 = webpage_getDepth(w1);
+	if(webpage_fetch(w1)){
+		printf("Webpage found and fetched\n");
+	} else{
+		printf("Error getting webpage");
+		exit(EXIT_FAILURE);
+	}
 	
-	printf("%d\n", w2);
+
+	// iterating over URLS
+	int pos=0;
+	char *result;
+	while((pos=webpage_getNextURL(w1, pos, &result)) > 0){
+		printf("Found url: %s\n", result);
+		free(result);
+	}
 	
-	printf("Hello -- Crawlerp--!\n");
+	
+	
+
 
 	webpage_delete(w1);
-	
 	exit(EXIT_SUCCESS);
 
 }

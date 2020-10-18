09076262086fb0d16e9bd109800fe86df93c6638
John Lass <John.J.Lass.TH@dartmouth.edu>
2020-10-18 02:33:52 -0400

    10.18.2020:2:33

--- tse/crawler/crawler.c
+++ tse/crawler/crawler.c
@@ -9,20 +9,19 @@
  * 
  */
 
+#include "webpage.h"
+#include "hash.h"
+#include "queue.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "queue.h"
-#include "hash.h"
-#include "webpage.h"
 #include <stdbool.h>
 
 int main(void){
 	char *url = "http://thayer.github.io/engs50/";
 	int depth = 0;
+	char *html = "This is a webpage with the URL http://thayer.github.io/engs50/";
 
- 
-
-	webpage_t *w1 = webpage_new(url,depth,NULL);
+	webpage_t *w1 = webpage_new(url,depth,html);
 
 	if(webpage_fetch(w1)){
 		printf("Webpage found and fetched\n");
@@ -30,18 +29,17 @@ int main(void){
 		printf("Error getting webpage");
 		exit(EXIT_FAILURE);
 	}
-	
 
+	
 	// iterating over URLS
 	int pos=0;
-	char *result;
+		char *result=NULL;
 	while((pos=webpage_getNextURL(w1, pos, &result)) > 0){
 		printf("Found url: %s\n", result);
 		free(result);
 	}
 	
-	
-	
+		
 
 
 	webpage_delete(w1);

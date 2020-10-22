f47f84fef44db2d9f4ff7a93d5b0870706993afb
BobbyCarangelo <robert.f.carangelo.iii.21@dartmouth.edu>
2020-10-22 18:19:29 -0400

    10.22.2020 6:19 pm est

---
+++ tse/indexer/indexer.c
@@ -0,0 +1,21 @@
+/* indexer.c --- 
+ * 
+ * 
+ * Author: Robert F. Carangelo III
+ * Created: Thu Oct 22 18:12:34 2020 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	printf("Hello\n");
+
+	exit(EXIT_SUCCESS);
+}
